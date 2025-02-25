2d3c6882b5fb0654139df8774ea7ac4a136b909f
dino0718 <dinoliuit@gmail.com>
2025-02-25 09:14:30 +0800

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,5 +1,7 @@
-sudo  apt install apache2 php php-xml php-json -y &> /dev/null
-echo "apache2 php installed"
+sudo  apt install apache2 -y &> /dev/null
+echo "apache2  installed"
+sudo apt install php php-xml pjp-json -y &> /dev/null 
+echo "php installed"
 wget https://github.com/phpsysinfo/phpsysinfo/archive/refs/tags/v3.4.4.zip &> /dev/null
 unzip v3.4.4.zip &> /dev/null
 sudo mv phpsysinfo-3.4.4/* /var/www/html/
